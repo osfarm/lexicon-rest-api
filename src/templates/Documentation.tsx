91e5aefaf14ac29d9e47f76d180c439d4228f6cd
Elliott Lépine <elliott@brad.ag>
2025-02-15 11:33:23 +0100

    Converts the Documentation template from plain HTML to JSX

--- src/templates/Documentation.tsx
+++ src/templates/Documentation.tsx
@@ -1,5 +1,4 @@
 import { Html } from "@elysiajs/html"
-import { Layout } from "./Layout"
 import { type HypermediaType } from "../Hypermedia"
 
 interface Props {
@@ -14,32 +13,30 @@ interface Props {
 }
 
 export function Documentation(props: Props) {
-  return `<!DOCTYPE html> <html lang="en">
-    <head>
+  return (
+    <html lang="en">
+      <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>${props.documentation.info.title}</title>
+        <title>{props.documentation.info.title}</title>
         <meta
-            name="description"
-            content="${props.documentation.info.description}"
+          name="description"
+          content={props.documentation.info.description}
         />
         <meta
-            name="og:description"
-            content="${props.documentation.info.description}"
-        />    
-    
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.19.5/swagger-ui.css" >
-        <style>
-          .topbar {
-            display: none;
-          }
-        </style>
-    </head>
-    <body>
+          name="og:description"
+          content={props.documentation.info.description}
+        />
+      </head>
+      <body>
+        <script id="api-reference">
+          {JSON.stringify(props.documentation)}
+        </script>
         <script
-      id="api-reference"
-    >${JSON.stringify(props.documentation)}
-    </script>
-    <script src="${`https://cdn.jsdelivr.net/npm/@scalar/api-reference@${"latest"}/dist/browser/standalone.min.js`}" crossorigin></script>
-    </body> </html>`
+          src={`https://cdn.jsdelivr.net/npm/@scalar/api-reference@${"latest"}/dist/browser/standalone.min.js`}
+          crossorigin="true"
+        ></script>
+      </body>
+    </html>
+  )
 }
